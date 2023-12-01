9f50431b5b9136af5f69ea3eefc4224939651e1f
Derek Lomax <derekthecool@gmail.com>
2023-12-01 16:18:20 -0700

    Success for day 1

--- 2023/dotnet/FsharpLibrary/Days.fs
+++ 2023/dotnet/FsharpLibrary/Days.fs
@@ -3,12 +3,21 @@
 open Helpers.AdventDataFetcher
 
 module Answers =
+    open System.Text.RegularExpressions
+
+    let calibrationRecovery input =
+        Regex.Matches(input, @"\d")
+        |> Seq.toArray
+        |> fun numbers -> ($"{numbers[0]}{numbers[numbers.Length - 1]}" |> int)
+
+
     let Day1 =
-        let tracer = (getFunctionInfo()).get()
+        let tracer = (getFunctionInfo ()).get ()
         printfn "tracer: %A" tracer
-        let input = getInputData tracer |> Seq.iter(fun line -> printfn "%A" line)
-        ()
+        let output = getInputData tracer |> Seq.map calibrationRecovery |> Seq.sum
+        printfn "Output: %A" output
+        output
 
     let Day2 =
-        let tracer = (getFunctionInfo()).get()
+        let tracer = (getFunctionInfo ()).get ()
         printfn "tracer: %A" tracer
