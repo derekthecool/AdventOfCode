861b7dcc97eaec098f9c14e58d6749f5abecb84a
Derek Lomax <derekthecool@gmail.com>
2024-12-11 14:31:13 -0700

    feat: work more on day3, find out why part 2 is failing

--- 2024/lua/day_3.lua
+++ 2024/lua/day_3.lua
@@ -8,6 +8,14 @@ M.extract_mul_raw = function(text)
     return output
 end
 
+M.remove_disabled_ranges = function(text)
+    -- This attempts to delete all text between don't and do
+    -- However, it will not work because it needs a half greedy solution
+    -- e.g. don't .... don't .... don't .... do will only delete the last don't to the do
+    -- the first few don'ts would not be deleted
+    return text:gsub("don't%(%).-do%(%)", "")
+end
+
 M.raw_mul_to_table = function(raw_mul)
     return map(function(item)
         local output = {}
@@ -20,6 +28,21 @@ end
 
 M.part_1 = function(input)
     local raw = M.extract_mul_raw(input)
+    local parsed = M.raw_mul_to_table(raw)
+    local sum = 0
+    for _, value in ipairs(parsed) do
+        sum = sum + value[1][1] * value[1][2]
+    end
+    return sum
+end
+
+M.part_2 = function(input)
+    local trimmed = M.remove_disabled_ranges(input)
+    -- print(trimmed)
+
+    local raw = M.extract_mul_raw(trimmed)
+    -- print(table.concat(raw,  '\n'))
+
     local parsed = M.raw_mul_to_table(raw)
     local sum = 0
     for _, value in ipairs(parsed) do
